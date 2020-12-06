c2a1f20a5a68b8e0e188a4bbcc62ba5af2933084
CG360 <CloudGamer360@users.noreply.github.com>
2020-12-06 16:27:36 +0000

    Added parser method for permission objects.

--- src/main/java/net/mooncraftgames/server/servertransferutils/quicktransfer/QTDestinationConfiguration.java
+++ src/main/java/net/mooncraftgames/server/servertransferutils/quicktransfer/QTDestinationConfiguration.java
@@ -1,18 +1,59 @@
 package net.mooncraftgames.server.servertransferutils.quicktransfer;
 
-import java.util.ArrayList;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import net.mooncraftgames.server.servertransferutils.types.PermissionStore;
+
+import java.io.File;
 
 public abstract class QTDestinationConfiguration {
 
-    private ArrayList<String> loadedConfigs;
+    private File configLocation;
+    private PermissionStore parentPermissionStore;
 
-    public QTDestinationConfiguration() {
-        this.loadedConfigs = new ArrayList<>();
+    public QTDestinationConfiguration(File configLocation) {
+        this.configLocation = configLocation;
     }
 
     //TODO: Multiple configs can be loaded cause why not.
     // - Each config can have a parent permission group defined.
     // - Each destination can have sub-perms.
+    public static PermissionStore parsePermissionStore(JsonElement permissions){ return parsePermissionStore(permissions, null); }
+    public static PermissionStore parsePermissionStore(JsonElement permissions, PermissionStore parent){
+        PermissionStore store = new PermissionStore(parent);
+
+        if(permissions instanceof JsonObject){
+            JsonObject storeObject = (JsonObject) permissions;
+            JsonElement requiredElement = storeObject.get("required");
+            JsonElement blockedElement = storeObject.get("blocked");
+            // !! does not load any parent entries from config. Those are internal.
+
+            if(requiredElement instanceof JsonArray){
+
+                for(JsonElement reqEl : (JsonArray) requiredElement){
+
+                    if(reqEl instanceof JsonPrimitive){
+                        JsonPrimitive reqPrim = (JsonPrimitive) reqEl;
+                        store.addRequiredPermission(reqPrim.getAsString());
+                    }
+                }
+            }
+
+            if(blockedElement instanceof JsonArray){
+                for(JsonElement blkEl : (JsonArray) blockedElement){
+
+                    if(blkEl instanceof JsonPrimitive){
+                        JsonPrimitive blkPrim = (JsonPrimitive) blkEl;
+                        store.addBlockedPermission(blkPrim.getAsString());
+                    }
+                }
+            }
+        }
+
+        return store;
+    }
 
     /*
 
