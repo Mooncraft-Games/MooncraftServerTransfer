1f5042b02d747566d3793c873a421e987032840e
CG360 <CloudGamer360@users.noreply.github.com>
2020-12-06 16:51:55 +0000

    Initial implementation of loading from file.

--- src/main/java/net/mooncraftgames/server/servertransferutils/quicktransfer/QTDestinationConfiguration.java
+++ src/main/java/net/mooncraftgames/server/servertransferutils/quicktransfer/QTDestinationConfiguration.java
@@ -1,12 +1,16 @@
 package net.mooncraftgames.server.servertransferutils.quicktransfer;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+import com.google.gson.*;
+import net.mooncraftgames.server.servertransferutils.ServerTransferUtils;
+import net.mooncraftgames.server.servertransferutils.types.Destination;
 import net.mooncraftgames.server.servertransferutils.types.PermissionStore;
+import net.mooncraftgames.server.servertransferutils.types.ProtectedDestination;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Handles configuration specifically for the QuickTransfer feature.
@@ -16,9 +20,46 @@ public abstract class QTDestinationConfiguration {
 
     private File configLocation;
     private PermissionStore parentPermissionStore;
+    private ArrayList<ProtectedDestination> destinations;
 
     public QTDestinationConfiguration(File configLocation) {
         this.configLocation = configLocation;
+        this.reloadFileFromDisk();
+    }
+
+    public boolean reloadFileFromDisk(){
+        if(configLocation.exists() && configLocation.isFile()){
+            try {
+                FileReader reader = new FileReader(configLocation);
+                BufferedReader read = new BufferedReader(reader);
+
+                String jsonText = "";
+                Iterator<String> r = read.lines().iterator();
+                while (r.hasNext()){
+                    jsonText = jsonText.concat(r.next());
+                }
+
+                JsonElement root = new JsonParser().parse(jsonText);
+                return applyJsonToConfig(root);
+
+            } catch (Exception err) {
+                ServerTransferUtils.getPlgLogger().emergency("There was an error reading the config: "+configLocation.getAbsolutePath());
+                err.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    protected boolean applyJsonToConfig(JsonElement rootElement){
+        if(rootElement instanceof JsonObject) {
+            JsonObject root = (JsonObject) rootElement;
+            JsonElement parentStore = root.get("permissions");
+            JsonElement destinationStore = root.get("destinations");
+
+            this.parentPermissionStore = parsePermissionStore(parentStore);
+            this.destinations = parseDestinations(destinationStore, parentPermissionStore);
+        }
+        return false;
     }
 
     //TODO: Multiple configs can be loaded cause why not.
@@ -57,6 +98,10 @@ public abstract class QTDestinationConfiguration {
         }
 
         return store;
+    }
+
+    private static ArrayList<ProtectedDestination> parseDestinations (JsonElement destinations, PermissionStore parent){
+        //TODO: load destinations.
     }
 
     /*
